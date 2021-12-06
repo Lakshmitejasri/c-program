af35276e2e52cbe2bd961641fb577bd404faceec
VU21CSEN0100121 <94819915+VU21CSEN0100121@users.noreply.github.com>
2021-12-06 08:14:34 -0800

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int p,t,r;
+	float s;
+	printf("enter the value for p",p);
+	scanf("%d",&p);
+	printf("enter the value for t",t);
+	scanf("%d",&t);
+	printf("enter the value for", r);
+	scanf("%d",&r);
+	s=(p*t*r/100);
+	printf("%fthe value of s is",s);
+}
