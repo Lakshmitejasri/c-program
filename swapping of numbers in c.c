bf65553a0bd094d16ac2d252ef684052423d34f7
VU21CSEN0100121 <94819915+VU21CSEN0100121@users.noreply.github.com>
2021-12-06 08:14:51 -0800

    Add files via upload

---
+++ swapping of numbers in c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,b,x;
+	printf("enter the value for a");
+	scanf("%d",&a);
+	printf("enter the value for b");
+	scanf("%d",&b);
+	x==a;
+	a==b;
+	b==x;
+	printf("value of a is%d",b);
+	printf("value of b is %d",a);	
+}
